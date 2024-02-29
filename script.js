b097b1444e415bc58471da40cf9cff5c6e7004ea
Johnvilin-Sibina <johnvilinsibina@gmail.com>
2024-02-29 14:39:07 +0530

    Fetch on 28/02/2024

--- script.js
+++ script.js
@@ -1,72 +1,26 @@
-/*var request= new XMLHttpRequest();
-request.open("GET","https://restcountries.com/v3.1/all")
-request.send();
-request.onload = function(){
-    var res = JSON.parse(request.response);
-    console.log(res);
-for(var i=0;i<res.length;i++){
-    //console.log(res[i].name.common);
-    if(res[i].capital){
-        console.log(res[i].capital[0]+" "+res[i].area)
-    }
-    else{
-        console.log("Invalid Capital"+" "+res[i].area)
-    }
-}
-}*/
-/*05/02/2024
-let request = new XMLHttpRequest();
-request.open("GET","https://raw.githubusercontent.com/rvsp/restcountries-json-data/master/res-countries.json");
-request.send();
-request.onload = function(){
-    let res = JSON.parse(request.response);
-    console.log(res);
-    //Print only those countries which belong to Asia region.
-    let asia = res.filter((ele)=>ele.region=="Asia");
-    console.log(asia);
-    let asian_countries = asia.map((ele)=>ele.name);
-    console.log(asian_countries);
-    //Print the capitals of those countries whose population is less than 2lakhs.
-    let popu = res.filter((ele)=>ele.population<200000);
-    console.log(popu);
-    let popu_countries = popu.map((ele)=>ele.capital);
-    console.log(popu_countries);
-    //Print the sum of total population of all the countries.
-    let pop = res.reduce((acc,cv)=>acc+cv.population,0);
-    console.log(pop);
-}*/
-/*13/02/24:
-Find the missing number in the array [1,2,3,5]
-Solution:
-let arr = [1,2,3,5];
-for(let i=0;i<arr.length-1;i++){
-  let dif = arr[i+1]-arr[i];
-if(dif==1){
-  continue;
-  }
-  else{
-console.log(arr[i]+1);
-}
-}
-let arr1 = [1,2,3,5]
-for(let i=0;i<arr1.length;i++){
-    if(arr[i]+1!=arr1[i+1]){
-        console.log(arr1[i]+1);
-        break;
-    }
+/* var res = fetch("https://restcountries.com/v3.1/all")
+.then((data)=>data.json()).then((data1)=>console.log(data1));*/
+// console.log(res);
 
-    }*/
-// Logical Operator:
-var a = 1;
-var b = 0;
-var c = 2;
-console.log(a || b);
-console.log(b || a);
-console.log(a && b);
-console.log(a && c);
-console.log(c && a);
+// var res = fetch("https://restcountries.com/v3.1/all").then((data)=>console.log(data.json()));
 
-var a=3;
-var b=-2;
-console.log(a>0&&b>0);
-console.log(a>0||b>0);
+/* var res1 = fetch("https://raw.githubusercontent.com/Rajavasanthan/jsondata/master/pagenation.json")
+.then((data)=>data.json()).then((data1)=>console.log(data1)); */
+
+/* var res2 = fetch("https://raw.githubusercontent.com/rvsp/restcountries-json-data/master/res-countries.json")
+.then((data)=>data.json()).then((data1)=>console.log(data1)); */
+
+var res = fetch("https://restcountries.com/v3.1/all")
+.then((data)=>data.json()).then((data1)=>bar(data1));
+
+function bar(data1){
+    for(var i=0;i<data1.length;i++){
+        var res = data1[i].latlng
+        foo(...res)
+    }
+}
+// here we are using open weather
+function foo(lat,lon){
+   var final_res = fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=5eb779a14d85a8b363191098dc26c5ef`)
+.then((data2)=>data2.json()).then((data3)=>console.log(data3.main.temp))
+}
